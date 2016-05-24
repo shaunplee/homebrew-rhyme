64bfe4d4173422c1fc2a055222608da3bda929bd
Shaun Lee <shaun.lee@gmail.com>
2016-05-23 23:41:13 -0700

    Fix url to https and shorten desc.

--- Formula/rhyme.rb
+++ Formula/rhyme.rb
@@ -1,7 +1,7 @@
 class Rhyme < Formula
-  desc "Takes a word and returns a formatted list of all words that rhyme with it."
+  desc "Takes a word and returns a formatted list of words that rhyme with it."
   homepage "http://rhyme.sourceforge.net"
-  url "http://downloads.sourceforge.net/project/rhyme/rhyme/0.9/rhyme-0.9.tar.gz?r=&ts=1464063085"
+  url "https://downloads.sourceforge.net/project/rhyme/rhyme/0.9/rhyme-0.9.tar.gz?r=&ts=1464063085"
   version "0.9"
   sha256 "11d4862cc3adfc18ea83ca233854c562fcebdc838fa7fb62de6ef3f63f992bd4"
 
